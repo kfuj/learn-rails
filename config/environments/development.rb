c6c0087d4d795e529da2cd2437e36fac65d33ac0
kfuj <sisterfuj@gmail.com>
2016-04-22 05:41:06 +0000

    add configuration- change back to host 3000

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,7 @@ Rails.application.configure do
   password: Rails.application.secrets.email_provider_password
 }
 # ActionMailer Config
-#config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-config.action_mailer.default_url_options = { :host => 'https://ide.c9.io/kfuj/learn-rails' }
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.raise_delivery_errors = true
 # Send email in development mode?
